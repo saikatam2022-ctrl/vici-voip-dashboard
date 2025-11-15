92aaca62df2c823f2fc427a31218bf4481c8d9d2
saikatam2022-ctrl <saikat.am2022@gmail.com>
2025-11-15 11:49:20 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -69,6 +69,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
     ],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
